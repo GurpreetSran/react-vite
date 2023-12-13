78c9eff1703a04d8cdc1419ceb4458c1a3497771
Gurpreet Singh <gurpreet.singh@trinnylondon.com>
2023-12-13 21:12:41 +0000

    fix game

--- src/assignments/memory/intex.tsx
+++ src/assignments/memory/intex.tsx
@@ -41,19 +41,22 @@ const MemoryGame: React.FC = () => {
   const [grid, setGrid] = useState(gridArray);
   const [guessedItems, setGuessedItems] = useState<SelectedItem[]>([]);
   const [selectedItems, setSelectedItems] = useState<SelectedItem[]>([]);
+  const [turns, setTurns] = useState(0);
 
   useEffect(() => {
     if (selectedItems.length === 2) {
       if (selectedItems[0].value === selectedItems[1].value) {
-        setGuessedItems([...guessedItems, ...selectedItems]);
+        setGuessedItems((guessedItems) => [...guessedItems, ...selectedItems]);
         alert("great job");
       } else {
         alert("Try again");
       }
 
       setSelectedItems([]);
+      setGrid(structuredClone(grid));
+      setTurns(turns + 1);
     }
-  }, [selectedItems, guessedItems]);
+  }, [selectedItems, guessedItems, grid, turns]);
 
   const onItemClick = (
     rowIndex: number,
@@ -75,42 +78,40 @@ const MemoryGame: React.FC = () => {
       },
     ]);
 
-    const x = [...grid];
-    setGrid(x);
-
     const el = (e.target as HTMLElement).querySelector("span");
 
     if (el) {
       el.style.visibility = "visible";
-      const timeout = setTimeout(() => {
+      setTimeout(() => {
         el.style.visibility = "hidden";
       }, 1000);
-
-      clearTimeout(timeout);
     }
   };
 
   return (
     <div className="container">
+      <h2>Tries: {turns}</h2>
       {grid.map((row, rowIndex) => {
         return (
           <div key={rowIndex}>
             {row.map((col, colIndex) => {
-              const isMatch = !!guessedItems.find((x) => x.value == col);
+              const isMatch = !!guessedItems.find((item) => item.value == col);
               return (
                 <>
                   <div
                     className="gridContainer"
+                    style={{ visibility: isMatch ? "hidden" : "visible" }}
                     key={`${rowIndex}${colIndex}`}
                     onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) =>
                       onItemClick(rowIndex, colIndex, e, isMatch, col)
                     }
                   >
-                    <span style={{ visibility: "hidden" }}>
-                      {isMatch ? "Matched" : col}
+                    <span
+                      style={{ visibility: isMatch ? "visible" : "hidden" }}
+                    >
+                      {col}
                     </span>
                   </div>
-                  {/* <div className="hideItem" /> */}
                 </>
               );
             })}
